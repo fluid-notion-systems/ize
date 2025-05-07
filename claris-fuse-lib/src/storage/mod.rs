7409362c0068c7e3468edc8999f6c86acae36422
Claris Assistant <assistantclaris@gmail.com>
2025-05-07 14:37:52 +1000

    Design SQLite schema for version history storage

--- claris-fuse-lib/src/storage/mod.rs
+++ claris-fuse-lib/src/storage/mod.rs
@@ -1,6 +1,9 @@
 mod traits;
+mod sqlite_schema;
 
 pub use traits::{
     FileVersion, OperationType, StorageError, StorageFactory, StorageResult, VersionStorage,
     VersionedFile,
 };
+
+pub use sqlite_schema::SqliteSchema;
