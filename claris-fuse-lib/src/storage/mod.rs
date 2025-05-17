daed13b201937fb168776fdc8c6212264ce0d2bf
Nicholas Hemsley <nick.hems@gmail.com>
2025-05-17 13:07:04 +1000

    remove old Sqlite code

--- claris-fuse-lib/src/storage/mod.rs
+++ claris-fuse-lib/src/storage/mod.rs
@@ -1,13 +1,8 @@
 pub mod diesel_sqlite;
 pub mod models;
-mod sqlite;
-mod sqlite_schema;
 mod traits;
 
 pub use traits::{
     FileVersion, OperationType, SearchableStorage, SearchableStorageFactory, StorageBackend,
     StorageError, StorageFactory, StorageResult, VersionStorage, VersionedFile,
 };
-
-pub use sqlite::{SqliteStorage, SqliteStorageFactory};
-pub use sqlite_schema::SqliteSchema;
